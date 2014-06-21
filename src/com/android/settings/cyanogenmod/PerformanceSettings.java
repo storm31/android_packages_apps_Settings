de76bc5ef08256641d165a8b24bbadc4884c5fa5
storm31 <sergej.michel@t-online.de>
2014-06-22 01:19:17 +0200

    Restore options to disable bootanimation and wallpaper

--- src/com/android/settings/cyanogenmod/PerformanceSettings.java
+++ src/com/android/settings/cyanogenmod/PerformanceSettings.java
@@ -54,12 +54,28 @@ public class PerformanceSettings extends SettingsPreferenceFragment implements
 
     private static final String USE_16BPP_ALPHA_PROP = "persist.sys.use_16bpp_alpha";
 
+    private static final String DISABLE_BOOTANIMATION_PREF = "pref_disable_bootanimation";
+
+    private static final String DISABLE_BOOTANIMATION_PERSIST_PROP = "persist.sys.nobootanimation";
+
+    private static final String DISABLE_BOOTANIMATION_DEFAULT = "0";
+
+    private static final String DISABLE_WALLPAPER_PREF = "pref_disable_wallpaperservice";
+
+    private static final String DISABLE_WALLPAPER_PERSIST_PROP = "persist.sys.show_wallpaper";
+
+    private static final String DISABLE_WALLPAPER_DEFAULT = "1";
+    
     private static final String FORCE_HIGHEND_GFX_PREF = "pref_force_highend_gfx";
     private static final String FORCE_HIGHEND_GFX_PERSIST_PROP = "persist.sys.force_highendgfx";
 
     private ListPreference mPerfProfilePref;
     private CheckBoxPreference mUse16bppAlphaPref;
     private CheckBoxPreference mForceHighEndGfx;
+
+    private CheckBoxPreference mDisableBootanimPref;
+
+    private CheckBoxPreference mDisableWallpaperPref;
 
     private String[] mPerfProfileEntries;
     private String[] mPerfProfileValues;
@@ -127,6 +143,18 @@ public class PerformanceSettings extends SettingsPreferenceFragment implements
             category.removePreference(findPreference(FORCE_HIGHEND_GFX_PREF));
         }
 
+        mDisableBootanimPref = (CheckBoxPreference) prefSet
+                .findPreference(DISABLE_BOOTANIMATION_PREF);
+        String disableBootanimation = SystemProperties.get(DISABLE_BOOTANIMATION_PERSIST_PROP,
+                DISABLE_BOOTANIMATION_DEFAULT);
+        mDisableBootanimPref.setChecked("1".equals(disableBootanimation));
+
+        mDisableWallpaperPref = (CheckBoxPreference) prefSet
+                .findPreference(DISABLE_WALLPAPER_PREF);
+        String disableWallpaper = SystemProperties.get(DISABLE_WALLPAPER_PERSIST_PROP,
+                DISABLE_WALLPAPER_DEFAULT);
+        mDisableWallpaperPref.setChecked("0".equals(disableWallpaper));
+
         /* Display the warning dialog */
         alertDialog = new AlertDialog.Builder(getActivity()).create();
         alertDialog.setTitle(R.string.performance_settings_warning_title);
@@ -170,6 +198,12 @@ public class PerformanceSettings extends SettingsPreferenceFragment implements
         } else if (preference == mForceHighEndGfx) {
             SystemProperties.set(FORCE_HIGHEND_GFX_PERSIST_PROP,
                     mForceHighEndGfx.isChecked() ? "true" : "false");
+        } else if (preference == mDisableBootanimPref) {
+            SystemProperties.set(DISABLE_BOOTANIMATION_PERSIST_PROP,
+                    mDisableBootanimPref.isChecked() ? "1" : "0");
+        } else if (preference == mDisableWallpaperPref) {
+            SystemProperties.set(DISABLE_WALLPAPER_PERSIST_PROP,
+                    mDisableWallpaperPref.isChecked() ? "0" : "1");
         } else {
             // If we didn't handle it, let preferences handle it.
             return super.onPreferenceTreeClick(preferenceScreen, preference);
